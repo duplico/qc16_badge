50af987958514f5583cd74afde0b0ba7cde071f0
George Louthan <duplico@dupli.co>
2019-06-10 02:05:55 -0500

    Add common CRC file.

---
+++ ccs_workspace/qc16_common/qc16_serial_common.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+#include "qc16_serial_common.h"
+
+uint16_t crc16_buf(volatile uint8_t *sbuf, uint8_t len) {
+    uint16_t crc=QC16_CRC_SEED;
+
+    while(len){
+        crc=(uint8_t)(crc >> 8) | (crc << 8);
+        crc^=(uint8_t) *sbuf;
+        crc^=(uint8_t)(crc & 0xff) >> 4;
+        crc^=(crc << 8) << 4;
+        crc^=((crc & 0xff) << 4) << 1;
+        len--;
+        sbuf++;
+    }
+    return crc;
+}
+
+uint16_t crc_build(uint8_t data, uint8_t new) {
+    static uint16_t crc = QC16_CRC_SEED;
+    if (new) {
+        crc16 = QC16_CRC_SEED;
+    }
+    crc=(uint8_t)(crc >> 8) | (crc << 8);
+    crc^=data;
+    crc^=(uint8_t)(crc & 0xff) >> 4;
+    crc^=(crc << 8) << 4;
+    crc^=((crc & 0xff) << 4) << 1;
+    return crc;
+}

82757950774950de80f5f7028d902a77f76b14da
George Louthan <georgerlouth@nthefourth.com>
2019-06-18 16:31:38 -0500

    Add the discrete all-LED cycling animation.

--- ccs_workspace/qbadge/ui/leds.c
+++ ccs_workspace/qbadge/ui/leds.c
@@ -6,24 +6,26 @@
  */
 
 #include <stdint.h>
+#include <string.h>
 
 #include <ti/sysbios/BIOS.h>
 #include <ti/sysbios/knl/Task.h>
 #include <ti/sysbios/knl/Event.h>
+#include <ti/sysbios/knl/Clock.h>
 
 #include "queercon_drivers/ht16d35b.h"
 #include "ui/leds.h"
 
 led_tail_anim_t led_tail_anim_current = {
     .speed=1,
-    .type=LED_TAIL_ANIM_TYPE_SCROLL,
+    .type=LED_TAIL_ANIM_TYPE_CYCLE,
     .colors= {
-              {0,0,0xffff},
-              {0,0,0xffff},
-              {0,0,0xffff},
-              {0,0,0xffff},
-              {0,0,0xffff},
-              {0,0,0xffff},
+              {255<<7, 0, 0},  // Red
+              {255<<7, 20<<7, 0}, // Orange
+              {255<<7, 60<<7, 0}, // Yellow
+              {0, 64<<7, 0},   // Green
+              {0, 0, 144<<7},  // Blue
+              {128<<7, 0, 96<<7}, // Purple
     }
 };
 
@@ -34,12 +36,17 @@ rgbcolor16_t led_tail_curr[6];
 rgbcolor16_t led_tail_dest[6];
 /// Step values of the dustbuster
 rgbdelta_t led_tail_step[6];
-
+uint16_t led_tail_steps_per_frame;
+uint16_t led_tail_frames_this_anim;
+uint16_t led_tail_frame_curr;
+uint16_t led_tail_step_curr;
 
 #define LED_STACKSIZE 1024
 Task_Struct led_task;
 uint8_t led_task_stack[LED_STACKSIZE];
 Event_Handle led_event_h;
+
+Clock_Handle led_tail_clock_h;
 
 rgbcolor16_t led_rainbow_colors[6] = {
     {255<<7, 0, 0},  // Red
@@ -61,9 +68,69 @@ void led_show_curr_colors() {
     Event_post(led_event_h, LED_EVENT_FLUSH);
 }
 
-// Call this from a task context when it's time to do a thing.
-void led_tail_timestep() {
+/// Start the current frame of the LED animation.
+void led_tail_frame_setup() {
+    switch(led_tail_anim_current.type) {
+    case LED_TAIL_ANIM_TYPE_CYCLE:
+        for (uint8_t i=0; i<6; i++) {
+            led_tail_curr[i].r = led_tail_anim_current.colors[led_tail_frame_curr].r;
+            led_tail_curr[i].g = led_tail_anim_current.colors[led_tail_frame_curr].g;
+            led_tail_curr[i].b = led_tail_anim_current.colors[led_tail_frame_curr].b;
 
+            // src, dest, and steps are DONTCARE because we're not fading.
+        }
+        break;
+    }
+
+    ht16d_put_colors(0, 6, led_tail_curr);
+    Event_post(led_event_h, LED_EVENT_FLUSH); // ready to show.
+
+    Clock_setTimeout(led_tail_clock_h, 100000); // TODO: this is just 1 second for now.
+    Clock_start(led_tail_clock_h);
+}
+
+void led_tail_timestep() {
+    led_tail_step_curr += 1;
+    if (led_tail_step_curr >= led_tail_steps_per_frame) {
+        led_tail_step_curr = 0;
+        led_tail_frame_curr += 1;
+        if (led_tail_frame_curr == led_tail_frames_this_anim) {
+            led_tail_frame_curr = 0;
+        }
+        led_tail_frame_setup();
+    } else {
+        switch(led_tail_anim_current.type) {
+        case LED_TAIL_ANIM_TYPE_CYCLE:
+            // we're not doing any stepping here because it's not one of
+            // the animations that fades.
+            break;
+        }
+    }
+
+    Clock_setTimeout(led_tail_clock_h, 100000); // TODO: this is just 1 second for now.
+    Clock_start(led_tail_clock_h);
+}
+
+void led_tail_start_anim() {
+    memset(led_tail_src, 0x00, sizeof(rgbcolor16_t)*6);
+    memset(led_tail_dest, 0x00, sizeof(rgbcolor16_t)*6);
+    memset(led_tail_src, 0x00, sizeof(rgbcolor16_t)*6);
+    memset(led_tail_step, 0x00, sizeof(rgbdelta_t)*6);
+    led_tail_frame_curr = 0;
+    led_tail_step_curr = 0;
+
+    switch(led_tail_anim_current.type) {
+    case LED_TAIL_ANIM_TYPE_CYCLE:
+        led_tail_steps_per_frame = 1;
+        led_tail_frames_this_anim = 6;
+        break;
+    }
+
+    led_tail_frame_setup();
+}
+
+void led_tail_step_swi(UArg a0) {
+    Event_post(led_event_h, LED_EVENT_TAIL_STEP);
 }
 
 void led_task_fn(UArg a0, UArg a1) {
@@ -72,6 +139,7 @@ void led_task_fn(UArg a0, UArg a1) {
 
     ht16d_init();
     ht16d_all_one_color(0,0,0);
+    led_tail_start_anim();
 
     while (1) {
         events = Event_pend(led_event_h, Event_Id_NONE, ~Event_Id_NONE,  BIOS_WAIT_FOREVER);
@@ -88,6 +156,10 @@ void led_task_fn(UArg a0, UArg a1) {
         if (events & LED_EVENT_FLUSH) {
             led_flush();
         }
+
+        if (events & LED_EVENT_TAIL_STEP) {
+            led_tail_timestep();
+        }
     }
 }
 
@@ -98,4 +170,10 @@ void led_init() {
     taskParams.stackSize = LED_STACKSIZE;
     taskParams.priority = 1;
     Task_construct(&led_task, led_task_fn, &taskParams, NULL);
+
+    Clock_Params clockParams;
+    Clock_Params_init(&clockParams);
+    clockParams.period = 0; // one-shot clock.
+    clockParams.startFlag = FALSE;
+    led_tail_clock_h = Clock_create(led_tail_step_swi, 100, &clockParams, NULL);
 }

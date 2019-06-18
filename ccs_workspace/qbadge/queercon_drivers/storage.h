680366400848387b754e911380c46dc47ab81a04
George Louthan <georgerlouth@nthefourth.com>
2019-06-17 21:08:17 -0500

    Compile in the SPIFFS code.

---
+++ ccs_workspace/qbadge/queercon_drivers/storage.h
@@ -0,0 +1,14 @@
+/*
+ * storage.h
+ *
+ *  Created on: Jun 17, 2019
+ *      Author: george
+ */
+
+#ifndef QUEERCON_DRIVERS_STORAGE_H_
+#define QUEERCON_DRIVERS_STORAGE_H_
+
+
+void storage_init();
+
+#endif /* QUEERCON_DRIVERS_STORAGE_H_ */

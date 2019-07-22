684bccc085a2e3563e89dfba91795cbc9beb382f
George Louthan <duplico@dupli.co>
2019-07-21 20:49:50 -0500

    Remote icon rendering working.

--- ccs_workspace/qbadge/ui/images.h
+++ ccs_workspace/qbadge/ui/images.h
@@ -27,6 +27,8 @@ extern const Graphics_Image img_pair_cb;
 extern const Graphics_Image img_pair_mission;
 extern const Graphics_Image img_pair_files;
 
+extern const Graphics_Image img_remote_icon_separator;
+
 extern const tImage *image_anim_type_buttons[];
 extern const tImage *image_mainmenu_icons[];
 extern const tImage *image_missionmenu_icons[];

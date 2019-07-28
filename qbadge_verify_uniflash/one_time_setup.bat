a1aefa74a2574b5ccbdd856f964f277f94b56301
George Louthan <duplico@dupli.co>
2019-07-28 17:50:43 -0500

    Add verification outputs.

---
+++ qbadge_verify_uniflash/one_time_setup.bat
@@ -0,0 +1,12 @@
+@echo off
+SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+
+  
+    CMD /C dpinst_64_eng.exe /SE /SW /SA /PATH %~dp0\ccs_base/emulation/drivers/stellaris/icdi
+  
+
+  
+    CMD /C dpinst_64_eng.exe /SE /SW /SA /PATH %~dp0\ccs_base/emulation/windows/xds110_drivers
+  

86b9ae05b24847c318c46e36562e0d4690026ece
George Louthan <georgerlouth@nthefourth.com>
2019-08-01 15:19:30 -0500

    Fix a stack overrun.

--- ccs_workspace/qbadge/ble/uble_bcast_scan.c
+++ ccs_workspace/qbadge/ble/uble_bcast_scan.c
@@ -39,7 +39,7 @@
 #define UBS_TASK_PRIORITY                     3
 
 #ifndef UBS_TASK_STACK_SIZE
-#define UBS_TASK_STACK_SIZE                   800
+#define UBS_TASK_STACK_SIZE                   1600
 #endif
 
 // RTOS Event to queue application events
